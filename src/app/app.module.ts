dd76032ab04348e8acb65d53b1cf84ce8e1aa2be
Bafana225 <121458472+Bafana225@users.noreply.github.com>
2023-03-24 13:36:50 +0100

    [feat] - Ajout Model+Client+Commande

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModelComponent } from './model/model.component';
+import { ClientComponent } from './client/client.component';
+import { CommandeComponent } from './commande/commande.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelComponent,
+    ClientComponent,
+    CommandeComponent
   ],
   imports: [
     BrowserModule,
